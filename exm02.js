d89f86c78f74fbab16aca915acba98c4e89bc65b
Gabriel Malosto <professional@gabdumal.com>
2022-06-28 19:51:15 -0300

    Renomeia arquivos

---
+++ exm02.js
@@ -0,0 +1,17 @@
+var num = 10;
+console.log(num + ": " + typeof num);
+
+var texto = "Isto é um texto!";
+console.log(texto + ": " + typeof texto);
+
+var logico = true;
+console.log(logico + ": " + typeof logico);
+
+var padrao;
+console.log(padrao + ": " + typeof padrao);
+
+var nulo = null;
+console.log(nulo + ": " + typeof nulo);
+
+var funcaoVar = function () {};
+console.log(funcaoVar + ": " + typeof funcaoVar);
