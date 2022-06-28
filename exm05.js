a5fe7d7a359cb293fae037974d5bb49c013ef653
Gabriel Malosto <professional@gabdumal.com>
2022-06-28 20:26:23 -0300

    Testa implementação de objeto literal

---
+++ exm05.js
@@ -0,0 +1,17 @@
+const pessoa = {
+  nome: "Fulano",
+  idade: 18,
+};
+
+// console.log(pessoa);
+// console.table(pessoa);
+// console.log(pessoa.idade, pessoa.nome);
+// console.log(pessoa["idade"], pessoa["nome"]);
+
+pessoa.altura = 1.82;
+pessoa["massa"] = 84.3;
+console.table(pessoa);
+
+pessoa = {
+  nada: "nada",
+};
