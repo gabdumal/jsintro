12ef18e29b1a8b7db7b0644f16295c04d6a97ac2
Gabriel Malosto <professional@gabdumal.com>
2022-07-19 20:55:14 -0300

    Trabalha com mais propriedades e estados

--- form01.js
+++ form01.js
@@ -1,6 +1,13 @@
+let estado = {
+  cor: "#000000",
+  altura: 0,
+  largura: 0,
+};
+
 function criaElemento(cor, largura, altura) {
   const eNovo = document.createElement("div");
   eNovo.textContent = `${cor} ${altura} ${largura} ${altura + largura}`;
+  eNovo.style = `color: ${cor}`;
   document.body.appendChild(eNovo);
 }
 
@@ -8,11 +15,12 @@ document.exm01.addEventListener("submit", submitListener);
 
 function submitListener(event) {
   event.preventDefault();
-  criaElemento(
-    document.exm01.cor.value,
-    Number(document.exm01.largura.value),
-    Number(document.exm01.altura.value)
-  );
+
+  estado.cor = document.exm01.cor.value;
+  estado.altura = Number(document.exm01.largura.value);
+  estado.largura = Number(document.exm01.altura.value);
+
+  criaElemento(estado.cor, estado.altura, estado.largura);
 
   document.exm01.reset();
 }
