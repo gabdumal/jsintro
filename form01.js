12253a3d8e536b28106f4b6eb5fe4e8d47a16b0e
Gabriel Malosto <professional@gabdumal.com>
2022-07-19 20:38:48 -0300

    Implementa operações pós-submit

---
+++ form01.js
@@ -0,0 +1,18 @@
+function criaElemento(cor, largura, altura) {
+  const eNovo = document.createElement("div");
+  eNovo.textContent = `${cor} ${altura} ${largura} ${altura + largura}`;
+  document.body.appendChild(eNovo);
+}
+
+document.exm01.addEventListener("submit", submitListener);
+
+function submitListener(event) {
+  event.preventDefault();
+  criaElemento(
+    document.exm01.cor.value,
+    Number(document.exm01.largura.value),
+    Number(document.exm01.altura.value)
+  );
+
+  document.exm01.reset();
+}
